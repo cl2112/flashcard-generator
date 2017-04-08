c1f721c879bd3390cbbbc361688f210c10690e05
cl2112 <cl2112cl2112@gmail.com>
2017-04-08 11:03:31 -0400

    got the labels changing on the site and the cards are being added to an array

--- flashcard.js
+++ flashcard.js
@@ -1,38 +1,75 @@
 $(document).ready(function(){
 
 
+var cardArray = [];
+
+var cardType = 1;
+
+
+
+
+$("#cardTypeDef").on("click", function(){
+	cardType = 1;
+	$("#labelFront").html("Term");
+	$("#labelBack").html("Definition");
+	$("#createBtn").html("Create Definition Flash Card");
+});
+
+$("#cardTypeCloze").on("click", function(){
+	cardType = 2;
+	$("#labelFront").html("Cloze");
+	$("#labelBack").html("Full Text");
+	$("#createBtn").html("Create Cloze Flash Card");
+});
+
 
 $("#createBtn").on("click", function () {
 	var cardTitle = $("#cardTitle").val().trim();
 	var cardFront = $("#cardFront").val().trim();
 	var cardBack = $("#cardBack").val().trim();
 	console.log(cardTitle, cardFront, cardBack);
-})
+
+	if (cardType === 1) {
+		var card = new BasicCard(cardTitle, cardFront, cardBack);
+		cardArray.push(card);
+		console.log(cardArray);
+	} else if (cardType === 2) {
+		var card = new ClozeCard(cardTitle, cardFront, cardBack);
+		cardArray.push(card);
+		console.log(cardArray);
+	} else {
+		console.log("Error!");
+	};	
+});
+
+
 
 
 
 
 // Basic flash card constructor
-function BasicCard(front, back) {
+function BasicCard(title,front, back) {
+	this.title = title;
 	this.front = front;
 	this.back = back;
 };
 
 
-var george = new BasicCard("President ONE?", "George!");
+// var george = new BasicCard("President ONE?", "George!");
 
-console.log(george.front);
-console.log(george.back);
+// console.log(george.front);
+// console.log(george.back);
 
 
 
 
 // Cloze flash card constructor
-function ClozeCard(text, cloze) {
+function ClozeCard(title, cloze, text) {
 	if (text.indexOf(cloze) === -1) {
 		console.log("ERROR - The CLOZE text is not in the full text.");
 		return "ERROR";
 	};
+	this.title = title;
 	this.cloze = cloze;
 	this.fullText = text;
 	this.partial = function() {
@@ -43,11 +80,11 @@ function ClozeCard(text, cloze) {
 
 // console.log("Broken");
 
-var ben = new ClozeCard("Ben Franklin was Great!", "Ben Franklin");
+// var ben = new ClozeCard("Ben Franklin was Great!", "Ben Franklin");
 
-console.log(ben.cloze);
-console.log(ben.fullText);
-console.log(ben.partial());
+// console.log(ben.cloze);
+// console.log(ben.fullText);
+// console.log(ben.partial());
 
 
 // ClozeCard should have a property or method that contains or returns only the cloze-deleted portion of the text.
